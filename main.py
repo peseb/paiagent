88029de718f2abe8332ce24ba7679bcd6f36f89e
Petter Bjørkås <pbjorkaas@enua.no>
2025-06-16 07:33:18 +0200

    Get prompt from args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,11 +8,17 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+prompt = ""
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+arguments = sys.argv
+if len(arguments) > 1:
+    prompt = arguments[1]
+else:
+    print(("No prompt given. Exiting..."))
+    exit(1)
+
 model = "gemini-2.0-flash-001"
 response = client.models.generate_content(model=model, contents=prompt)
-
 print("response: ", response.text)
 print("Prompt tokens:", response.usage_metadata.prompt_token_count)
 print("Response tokens:", response.usage_metadata.candidates_token_count)
