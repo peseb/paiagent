21872ac8dc3e4cf0dc5fd5b9a9e167823e980145
Petter Bjørkås <pbjorkaas@enua.no>
2025-06-17 07:42:54 +0200

    Add function tool

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ user_prompt = ""
 
 arguments = sys.argv
 if len(arguments) > 1:
-    prouser_promptmpt = arguments[1]
+    user_prompt = arguments[1]
 else:
     print(("No prompt given. Exiting..."))
     exit(1)
@@ -23,13 +23,48 @@ verbose = False
 if len(arguments) > 2 and arguments[2] == "--verbose":
     verbose = True
 
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
 model = "gemini-2.0-flash-001"
-response = client.models.generate_content(model=model, contents=messages)
-print("response: ", response.text)
+response = client.models.generate_content(model=model, contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
+if response.function_calls:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print("response: ", response.text)
+
 if verbose:
     print("User prompt: {user_prompt}")
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
